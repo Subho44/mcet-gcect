d087a7eb09473d94bcced798ef4e24aa390843a4
Subho44 <98902921+Subho44@users.noreply.github.com>
2025-08-28 18:21:02 +0530

    done
    done

--- Mutal-Fund-Tracking/Client/src/App.jsx
+++ Mutal-Fund-Tracking/Client/src/App.jsx
@@ -1,27 +1,35 @@
 import React from 'react'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Service from './pages/Service'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import PrivateRoute from './utils/PrivateRoute'
 
 const App = () => {
 
 
   return <>
-  <BrowserRouter>
-  <Header/>
-    <Routes>
-      <Route path='/' element={<Home/>}></Route>
-       <Route path='/about' element={<About/>}></Route>
-        <Route path='/contact' element={<Contact/>}></Route>
-         <Route path='/service' element={<Service/>}></Route>
-    </Routes>
-    <Footer/>
-  </BrowserRouter>
-   
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Register />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route element={<PrivateRoute />}>
+          <Route path='/home' element={<Home />}></Route>
+          <Route path='/about' element={<About />}></Route>
+          <Route path='/contact' element={<Contact />}></Route>
+          <Route path='/service' element={<Service />}></Route>
+        </Route>
+
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+
   </>
 }
 
--- Mutal-Fund-Tracking/Client/src/components/Header.jsx
+++ Mutal-Fund-Tracking/Client/src/components/Header.jsx
@@ -1,17 +1,34 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 const Header = () => {
+    const navigate = useNavigate();
+    const token = localStorage.getItem('token');
 
+    const hl = () => {
+        localStorage.removeItem('token');
+        navigate('/login');
+    }
     return <>
         <div>
             <h1>Mutal Funds</h1>
         </div>
         <div>
             <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-                <li><Link to="/service">Service</Link></li>
+                <li><Link to="/">Register</Link></li>
+                <li><Link to="/login">Login</Link></li>
+
+                {
+                    token && (
+                        <>
+                            <li><Link to="/home">Home</Link></li>
+                            <li><Link to="/about">About</Link></li>
+                            <li><Link to="/contact">Contact</Link></li>
+                            <li><Link to="/service">Service</Link></li>
+                            <button onClick={hl}>Logout</button>
+                        </>
+                    )
+                }
+
             </ul>
         </div>
     </>
